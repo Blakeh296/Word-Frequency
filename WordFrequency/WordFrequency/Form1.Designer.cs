a8fe16f40317c338dd4041e65d5dc476608e048b
Blake Hoelle <bhoelle216@gmail.com>
2018-05-30 19:53:34 -0400

    Commit

--- WordFrequency/WordFrequency/Form1.Designer.cs
+++ WordFrequency/WordFrequency/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tbOutPut = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,19 +48,21 @@
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.Color.LightGoldenrodYellow;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(84, 19);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(273, 22);
+            this.textBox1.Size = new System.Drawing.Size(268, 22);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(363, 19);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(358, 19);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(80, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Select a File";
+            this.button1.Text = "Pick a File";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -71,17 +74,32 @@
             this.dataGridView1.Size = new System.Drawing.Size(426, 200);
             this.dataGridView1.TabIndex = 3;
             // 
+            // tbOutPut
+            // 
+            this.tbOutPut.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.tbOutPut.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOutPut.Location = new System.Drawing.Point(12, 255);
+            this.tbOutPut.Name = "tbOutPut";
+            this.tbOutPut.Size = new System.Drawing.Size(426, 186);
+            this.tbOutPut.TabIndex = 5;
+            this.tbOutPut.Text = "";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 290);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(450, 453);
+            this.Controls.Add(this.tbOutPut);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Word Frequency";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -94,6 +112,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox tbOutPut;
     }
 }
 
--- WordFrequency/WordFrequency/Form1.cs
+++ WordFrequency/WordFrequency/Form1.cs
@@ -18,6 +18,21 @@ namespace WordFrequency
             InitializeComponent();
         }
 
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Adjust form size
+            this.Size = new Size(466, 89);
+
+            // Make these items initially invisible
+            label1.Visible = false;
+            textBox1.Visible = false;
+            dataGridView1.Visible = false;
+            tbOutPut.Visible = false;
+
+            // Put button in position
+            button1.Location = new Point(188, 19);
+        }
+
         string filePath = "";
 
         private void button1_Click(object sender, EventArgs e)
@@ -32,38 +47,55 @@ namespace WordFrequency
 
             int counter = 0;        // Counter for the While loop
 
-            // Limit the file types that the Dialog box can select from
-            open.Filter = "CSV Files| *.csv| Text Files (*.txt)| *.txt| All Files (*.*)|*.*";
-
-            open.Title = "Select a file to continue";     // Text that will display at the top of the Dialog box
-            open.InitialDirectory = path;       // tell the dialog box where to open from the variable set above
-            open.CheckFileExists = true;        // Makes sure the file exists
-            open.CheckPathExists = true;        // Makes sure the path exists
-
-            // Create some variables
-            int characterCount, wordsCount = 0;
-
-            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            try
             {
-                // Display the file path 
-                textBox1.Text = open.FileName;
-                // Save the file path from out NameSpace variable
-                filePath = textBox1.Text;
+                // Limit the file types that the Dialog box can select from
+                open.Filter = "CSV Files| *.csv| Text Files (*.txt)| *.txt| All Files (*.*)|*.*";
+
+                open.Title = "Select a file to continue";     // Text that will display at the top of the Dialog box
+                open.InitialDirectory = path;       // tell the dialog box where to open from the variable set above
+                open.CheckFileExists = true;        // Makes sure the file exists
+                open.CheckPathExists = true;        // Makes sure the path exists
+
+                if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    // Display the file path 
+                    textBox1.Text = open.FileName;
+                    // Save the file path from out NameSpace variable
+                    filePath = textBox1.Text;
+
+                    button1.Location = new Point(363, 19);  // Adjust button positioning
+
+                    dataGridView1.Visible = true;   // Make visable
+                    tbOutPut.Visible = true;        // Make visable
+                    textBox1.Visible = true;        // Make visable
+                    label1.Visible = true;          // Make visable
+
+                    this.Size = new Size(466, 491);               // Adjust the form size
+                }
+
+                sr = File.OpenText(filePath);       // Set the StreamReader now that you have the file path
+
+                string delim = ",";     // Create a delimiter
+
+                // An array and string variable for use in the while loop
+                string[] fields = null;
+                string line = null;
+
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    tbOutPut.Text = tbOutPut.Text + line + " ";
+                }
+
+                // Create the columns for the DataGrid
+                gridView.Columns.Add(new DataColumn("Word", typeof(string)));
+                gridView.Columns.Add(new DataColumn("Count", typeof(string)));
             }
-            
-            sr = File.OpenText(filePath);       // Set the StreamReader now that you have the file path
-
-            string delim = ",";     // Create a delimiter
-
-            // An array and string variable for use in the while loop
-            string[] fields = null;
-            string line = null;
-            
-            // Create the columns for the DataGrid
-            gridView.Columns.Add(new DataColumn("Word", typeof(string)));
-            gridView.Columns.Add(new DataColumn("Count", typeof(string)));
-
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
-
     }
 }
